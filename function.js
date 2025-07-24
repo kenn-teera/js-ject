7e0343834def719bc1bb25bee01550959744931f
Teerapat Yajai <teerapat.yj@gmail.com>
2025-07-24 23:07:19 +0700

    learn about function

---
+++ function.js
@@ -0,0 +1,9 @@
+//Function
+function myFunction(num1,num2){
+    let name = "keninsidefunc"; // can use only function
+    console.log(name)
+    return num1 * num2;
+}
+
+let sum = myFunction(10,150);
+console.log(sum);
