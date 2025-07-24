9c78e01642593aba58f65c7c0792630e61855d28
Teerapat Yajai <teerapat.yj@gmail.com>
2025-07-24 23:08:00 +0700

    learn about operator

---
+++ operators.js
@@ -0,0 +1,39 @@
+//Operators
+//Arithmetic Operators (num string variable)
+
+//Data Types var can store all datatypes
+// String
+let cat = "meow";
+let cat2 = 'meow';
+
+// Number
+let length = 200; // int
+let weight = 10.5; // float
+
+// Biglnt cann't be float cann't use math just storage 
+let bigInt = BigInt("10182394710923847109238470192347102394871203471023478012934780098123470975");
+
+// Boolean T/F
+let isTrue = true;
+let isFelse = false;
+
+// undefined
+let x;
+console.log(typeof x)
+
+// null
+let age = null;
+
+// Symbol unique use with object
+const sym1 = Symbol("Symbol1");
+const sym2 = Symbol("Symbol1");
+console.log(sym1===sym2);
+
+// Object
+const person = {
+    firstName: "Teera",
+    lastName: "Ya"
+};
+
+// Array
+const cars = ["A","B","C"];
