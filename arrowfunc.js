9fe86e197611f15145630dc99a4af5d2095b5c07
Teerapat Yajai <teerapat.yj@gmail.com>
2025-07-26 23:21:12 +0700

    learn about arrowfunc

---
+++ arrowfunc.js
@@ -0,0 +1,12 @@
+//arrow function
+
+//normal
+function add10(a,b){
+    return a+b;
+}
+console.log(add10(5,10),"normal function");
+
+//arrow function
+//arrow const var = (parameter) => {do something return}
+const add11 = (a,b) => a + b;
+console.log(add11(5,10),"arrow function");
